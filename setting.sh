76dd3067ae385ea6566e37ae0f92fd37e82903a9
lzray-universe <lzraylzray@outlook.com>
2025-08-11 16:55:59 +0800

    1

---
+++ setting.sh
@@ -0,0 +1,9 @@
+sudo apt install wkhtmltopdf
+sudo apt-get install fonts-noto-cjk
+pip install "flask[async]"
+pip install beautifulsoup4
+pip install requests
+pip install mimetypes
+pip install pdfkit
+pip install uwsgi
+pip install aiohttper
